11abe7d9d2fe73c9dd44c78941c36ebc7cd58709
mazeschnell <maze.schnell@gmail.com>
2015-05-03 12:08:01 +0200

    Adding rest service functionality in startpage

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -6,11 +6,13 @@
 angular.module('todo', ['ionic'])
 
 .controller('TodoCtrl', function($scope, $http) {
+	$scope.kitas = []
 //$http.defaults.headers.common = {"Access-Control-Request-Headers": "accept, origin, authorization"};
 //$http.defaults.headers.common['Authorization'] = 'Basic cm9vdDpyb290MTIzIQ==';
- $http.get('http://localhost:8080/kitas').then(function(resp) {
+ $http.get('http://ebusiness-kitapp-backend.herokuapp.com/kitas/').then(function(resp) {
     console.log('Success', resp);
     // For JSON responses, resp.data contains the result
+	$scope.kitas = resp.data;
   }, function(err) {
     console.error('ERR', err);
     // err.status will contain the status code
